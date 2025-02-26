644ebf90b972614d14c42492787ef4077848979a
Stas Petrov <petrovstas@mail.ru>
2025-02-26 16:32:48 +0500

    feat: add simple users controller

---
+++ src/users/users.controller.ts
@@ -0,0 +1,27 @@
+import {Body, Controller, Post} from "@nestjs/common"
+
+export class UserDto {
+    readonly id?: number
+    readonly name?: string
+    readonly email?: string
+    readonly password?: string
+
+    constructor(data: Partial<UserDto>) {
+        this.id = Number(data?.id) || undefined
+        this.name = data?.name ? data.name.toString() : undefined
+        this.email = data?.email ? data.email.toString() : undefined
+        this.password = data?.password ? data.password.toString() : undefined
+    }
+}
+
+@Controller("users")
+export class UsersController {
+
+    @Post()
+    create(@Body() data: Partial<UserDto>) {
+        const userData = new UserDto(data)
+        console.log(data)
+        console.log(userData)
+        // return this.usersService.create(userDto)
+    }
+}
