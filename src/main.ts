ed642215801e6bcc1fdb6e51a293241ab299a6ce
Stas Petrov <petrovstas@mail.ru>
2025-02-27 18:39:19 +0500

    feat: env-default & read port from env

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
-import { NestFactory } from "@nestjs/core"
-import { AppModule } from "./app.module"
+import {NestFactory} from "@nestjs/core"
+import {AppModule} from "./app.module"
+import {ValidationPipe} from "@nestjs/common"
+import {ConfigService} from "@nestjs/config"
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
-  await app.listen(3000)
+    const app = await NestFactory.create(AppModule)
+    // app.useGlobalPipes(new ValidationPipe())
+    const configService = app.get(ConfigService)
+    const port = configService.get<string>("APP_PORT")
+    await app.listen(port)
 }
+
 bootstrap()
