917cf8363c529cbebc05f467040a8f60eebe70a1
Stas Petrov <petrovstas@mail.ru>
2025-03-02 17:52:52 +0500

    refactor: remove unused code

--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -1,8 +1,7 @@
-import { Body, Controller, Param, ParseIntPipe, Patch, Post, Put, UsePipes, ValidationPipe } from '@nestjs/common';
+import {Body, Controller, Param, ParseIntPipe, Patch, Post, Put, UsePipes, ValidationPipe} from '@nestjs/common';
 import {UserService} from "./user.service"
 import {User} from "./user.entity"
-import { UserDto, ValidationGroup } from './user.dto';
-import { CustomUpdateUserDto } from './custom-update-user.dto';
+import {UserDto, ValidationGroup} from './user.dto';
 
 @Controller("users")
 export class UserController {
@@ -14,21 +13,15 @@ export class UserController {
     }
 
     @Post()
-    // @UsePipes(ValidationPipe)
     @UsePipes(new ValidationPipe({ groups: [ValidationGroup.CREATE] }))
     async create(@Body() data: UserDto): Promise<User> {
-        console.log(data)
         return await this.userService.create(data)
     }
 
     // обновляет всю модель
     @Put(":id")
-    // @UsePipes(new ValidationPipe({ groups: [ValidationGroup.UPDATE] }))
-    // @UsePipes(ValidationPipe)
-    // TODO: протестировать как будет работать в 2х вариантах UsePipes
+    @UsePipes(new ValidationPipe({ groups: [ValidationGroup.UPDATE] }))
     async update(@Param("id", ParseIntPipe) id: number, @Body() data: UserDto): Promise<User> {
-        console.log(id)
-        console.log(data)
         return await this.userService.update(id, data)
     }
 
@@ -36,9 +29,6 @@ export class UserController {
     @Patch(":id")
     @UsePipes(new ValidationPipe({ groups: [ValidationGroup.PARTIAL_UPDATE] }))
     async partialUpdate(@Param("id", ParseIntPipe) id: number, @Body() data: UserDto): Promise<void> {
-        console.log("custom")
-        console.log(id)
-        console.log(data)
         await this.userService.update(id, data)
     }
 }
