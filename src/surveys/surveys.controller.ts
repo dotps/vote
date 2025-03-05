520b691ea266c4e6222ba671264091d643a1abe3
Stas Petrov <petrovstas@mail.ru>
2025-03-05 17:04:28 +0500

    feat: add get all surveys

--- src/surveys/surveys.controller.ts
+++ src/surveys/surveys.controller.ts
@@ -1,4 +1,4 @@
-import {Body, Controller, Post, UsePipes, ValidationPipe} from '@nestjs/common';
+import {Body, Controller, Get, Post, UsePipes, ValidationPipe} from '@nestjs/common';
 import {SurveysService} from "./surveys.service"
 import {AnswerDto, SurveyDto} from "./surveys.dto"
 import {Public} from "../auth/public.decorator"
@@ -27,15 +27,18 @@ import { Survey } from "./survey.entity"
 export class SurveysController {
 
     constructor(private surveysService: SurveysService) {}
-    // Авторизованный пользователь может создать новый опрос,
-    // заполнив название, описание, вопросы и варианты ответов.
-    // Опрос сохраняется в базе данных и становится доступным для других пользователей.
+
     @Public() // TODO: убрать после завершения модуля
     @Post()
     // @UsePipes(new ValidationPipe({ groups: [ValidationGroup.CREATE] }))
     @UsePipes(ValidationPipe)
     async create(@Body() data: SurveyDto): Promise<Survey> {
-        console.log(data)
         return await this.surveysService.createSurvey(data)
+    }
+
+    @Public() // TODO: убрать после завершения модуля
+    @Get()
+    async getAll(): Promise<Survey[]> {
+        return await this.surveysService.getAll()
     }
 }
--- src/surveys/surveys.service.ts
+++ src/surveys/surveys.service.ts
@@ -1,19 +1,25 @@
-import { Injectable } from '@nestjs/common';
-import { SurveyDto } from "./surveys.dto"
-import { InjectRepository } from "@nestjs/typeorm"
-import { Repository } from "typeorm"
-import { Survey } from "./survey.entity"
+import {Injectable, NotFoundException} from "@nestjs/common"
+import {SurveyDto} from "./surveys.dto"
+import {InjectRepository} from "@nestjs/typeorm"
+import {Repository} from "typeorm"
+import {Survey} from "./survey.entity"
 
 @Injectable()
 export class SurveysService {
 
-  constructor(
-    @InjectRepository(Survey)
-    private readonly repository: Repository<Survey>,
-  ) {}
+    constructor(
+        @InjectRepository(Survey)
+        private readonly repository: Repository<Survey>,
+    ) {}
 
-  async createSurvey(data: SurveyDto): Promise<Survey> {
-    const survey = this.repository.create(data)
-    return await this.repository.save(survey)
-  }
+    async createSurvey(data: SurveyDto): Promise<Survey> {
+        const survey = this.repository.create(data)
+        return await this.repository.save(survey)
+    }
+
+    async getAll(): Promise<Survey[]> {
+        const surveys = await this.repository.find()
+        if (surveys.length === 0) throw new NotFoundException()
+        return await this.repository.find()
+    }
 }
