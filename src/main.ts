3c1ac7ae560febcb5568d84f10dbc306dfe76c1d
Stas Petrov <petrovstas@mail.ru>
2025-03-14 17:56:21 +0500

    TODO: можно ли сделать единый декоратор @ApiCreateUser() для описания api

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,29 @@ import {NestFactory} from "@nestjs/core"
 import {AppModule} from "./app.module"
 import {ValidationPipe} from "@nestjs/common"
 import {ConfigService} from "@nestjs/config"
-import { Config } from './config/config';
+import {Config} from "./config/config"
 import {SimpleLogger} from "./errors/SimpleLogger"
+import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger"
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule)
+
     app.useGlobalPipes(new ValidationPipe({transform: true, whitelist: true}))
     app.useLogger(new SimpleLogger(true))
+
     const configService = app.get(ConfigService)
     const port = configService.get<string>(Config.APP_PORT)
+
+    const configSwagger = new DocumentBuilder()
+        .setTitle("Survey API")
+        .setDescription("Описание API")
+        .setVersion("1.0")
+        // .addTag("пример")
+        .build()
+
+    const document = SwaggerModule.createDocument(app, configSwagger)
+    SwaggerModule.setup("api", app, document)
+
     await app.listen(port)
 }
 
--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -10,15 +10,17 @@ import {
     UsePipes,
     ValidationPipe
 } from "@nestjs/common"
-import { UserService } from "./user.service"
-import { User } from "./user.entity"
-import { UserDto } from "./user.dto"
-import { Public } from "../auth/public.decorator"
-import { ValidationGroup } from "../validation/ValidationGroup"
+import {UserService} from "./user.service"
+import {User} from "./user.entity"
+import {UserDto} from "./user.dto"
+import {Public} from "../auth/public.decorator"
+import {ValidationGroup} from "../validation/ValidationGroup"
 import {AuthDto} from "../auth/auth.dto"
 import {CurrentUser} from "./current-user.decorator"
 import {ErrorsMessages} from "../errors/errors"
+import {ApiBody, ApiOperation, ApiResponse, ApiTags} from "@nestjs/swagger"
 
+@ApiTags("users")
 @Controller("users")
 export class UserController {
 
@@ -27,13 +29,19 @@ export class UserController {
 
     @Public()
     @Post()
-    @UsePipes(new ValidationPipe({ groups: [ValidationGroup.CREATE] }))
+    @UsePipes(new ValidationPipe({groups: [ValidationGroup.CREATE]}))
+    @ApiOperation({summary: "Создание нового пользователя."})
+    @ApiResponse({status: 201, description: "Пользователь успешно создан.", type: AuthDto})
+    @ApiResponse({status: 400, description: "Неверные данные."})
+    @ApiBody({type: UserDto})
+    // TODO: продолжить делать документацию swagger
+        // TODO: можно ли сделать единый декоратор @ApiCreateUser() для описания api
     async create(@Body() data: UserDto): Promise<AuthDto> {
         return await this.userService.createUser(data)
     }
 
     @Put(":id")
-    @UsePipes(new ValidationPipe({ groups: [ValidationGroup.UPDATE] }))
+    @UsePipes(new ValidationPipe({groups: [ValidationGroup.UPDATE]}))
     async update(
         @Param("id", ParseIntPipe) id: number,
         @Body() data: UserDto,
@@ -44,7 +52,7 @@ export class UserController {
     }
 
     @Patch(":id")
-    @UsePipes(new ValidationPipe({ groups: [ValidationGroup.PARTIAL_UPDATE] }))
+    @UsePipes(new ValidationPipe({groups: [ValidationGroup.PARTIAL_UPDATE]}))
     async partialUpdate(
         @Param("id", ParseIntPipe) id: number,
         @Body() data: UserDto,
