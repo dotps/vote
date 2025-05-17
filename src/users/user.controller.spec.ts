88853e067f9e2c08274ef67ab24c411286450d7c
Stas Petrov <petrovstas@mail.ru>
2025-05-17 13:42:27 +0500

    feat: add test for create user

--- src/users/user.controller.spec.ts
+++ src/users/user.controller.spec.ts
@@ -1,125 +1,165 @@
-import {Test, TestingModule} from "@nestjs/testing"
-import {UserController} from "./user.controller"
-import {UserService} from "./user.service"
-import {User} from "./user.entity"
-import {UserDto} from "./user.dto"
-import {AuthDto} from "../auth/auth.dto"
-import {ForbiddenException} from "@nestjs/common"
+import { Test, TestingModule } from "@nestjs/testing"
+import { UserController } from "./user.controller"
+import { UserService } from "./user.service"
+import { User } from "./user.entity"
+import { UserDto } from "./user.dto"
+import { AuthDto } from "../auth/auth.dto"
+import { ForbiddenException, ValidationPipe, BadRequestException } from "@nestjs/common"
+import { ValidationGroup } from "../validation/ValidationGroup"
+import { APP_PIPE } from "@nestjs/core"
 
-describe("UserController", () => {
-    let controller: UserController
-    let userService: jest.Mocked<UserService>
+describe("UserController: ", () => {
+  let controller: UserController
+  let userService: jest.Mocked<UserService>
+  let validationPipe: ValidationPipe
 
-    const createMockUser = (id: number): User => ({
-        id,
-        name: "Test User",
-        email: "test@test.ru",
-        password: "password",
-        surveyResults: [],
-        isSelf: function(id: number): boolean {
-            return this.id === id
-        }
-    })
+  const createMockUser = (id: number): User => ({
+    id,
+    name: "Test User",
+    email: "test@test.ru",
+    password: "password",
+    surveyResults: [],
+    isSelf: function(id: number): boolean {
+      return this.id === id
+    },
+  })
 
-    const currentUserId = 1
-    const otherUserId = 2
-    const mockUser = createMockUser(currentUserId)
+  const currentUserId = 1
+  const otherUserId = 2
+  const mockCurrentUser = createMockUser(currentUserId)
 
-    const mockAuthDto = new AuthDto(mockUser, "test-token")
+  const mockAuthDto = new AuthDto(mockCurrentUser, "test-token")
 
-    const mockUserDto: UserDto = {
-        name: "Test User",
-        email: "test@test.ru",
-        password: "password"
+  let mockUserDto: UserDto = {
+    name: "Test User",
+    email: "test@test.ru",
+    password: "password",
+  }
+
+  beforeEach(async () => {
+    console.log("======================================================================")
+    console.log(expect.getState().currentTestName)
+
+    const mockUserService = {
+      createUser: jest.fn(),
+      updateUser: jest.fn(),
     }
 
-    beforeEach(async () => {
-        const mockUserService = {
-            createUser: jest.fn(),
-            updateUser: jest.fn(),
-        }
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [UserController],
+      providers: [
+        {
+          provide: UserService,
+          useValue: mockUserService,
+        },
+        ValidationPipe,
+      ],
+    }).compile()
 
-        const module: TestingModule = await Test.createTestingModule({
-            controllers: [UserController],
-            providers: [
-                {
-                    provide: UserService,
-                    useValue: mockUserService
-                }
-            ]
-        }).compile()
+    controller = module.get<UserController>(UserController)
+    userService = module.get(UserService)
+    // validationPipe = new ValidationPipe()
+    validationPipe = new ValidationPipe({
+      exceptionFactory: (errors) => {
+        console.log("Ошибки валидации:", JSON.stringify(errors, null, 2))
+        return new BadRequestException(errors)
+      },
+    })
+  })
 
-        controller = module.get<UserController>(UserController)
-        userService = module.get(UserService)
+  describe("создание пользователя: ", () => {
+    it("создать нового пользователя", async () => {
+      userService.createUser.mockResolvedValue(mockAuthDto)
+      const result = await controller.create(mockUserDto)
+      expect(userService.createUser).toHaveBeenCalledWith(mockUserDto)
+      expect(result).toEqual(mockAuthDto)
+      console.log("Ответ", result)
     })
 
-    // TODO: продолжить создание теста
+    describe("валидация данных", () => {
+      it("ошибка валидации при пустом email", async () => {
+        const invalidUserDto = { ...mockUserDto, email: "" }
+        await expect(validationPipe.transform(invalidUserDto, { type: "body", metatype: UserDto }))
+          .rejects
+          .toThrow(BadRequestException)
+      })
 
-    describe("создание пользователя", () => {
-        it("создать нового пользователя", async () => {
-            userService.createUser.mockResolvedValue(mockAuthDto)
+      it("ошибка валидации при некорректном email", async () => {
+        const invalidUserDto = { ...mockUserDto, email: "email" }
+        await expect(validationPipe.transform(invalidUserDto, { type: "body", metatype: UserDto }))
+          .rejects
+          .toThrow(BadRequestException)
+      })
 
-            const result = await controller.create(mockUserDto)
+      it("ошибка валидации при пустом имени", async () => {
+        const invalidUserDto = { ...mockUserDto, name: "" }
+        await expect(validationPipe.transform(invalidUserDto, { type: "body", metatype: UserDto }))
+          .rejects
+          .toThrow(BadRequestException)
+      })
 
-            expect(userService.createUser).toHaveBeenCalledWith(mockUserDto)
-            expect(result).toEqual(mockAuthDto)
-        })
+      it("ошибка валидации при пустом пароле", async () => {
+        const invalidUserDto = { ...mockUserDto, password: "" }
+        await expect(validationPipe.transform(invalidUserDto, { type: "body", metatype: UserDto }))
+          .rejects
+          .toThrow(BadRequestException)
+      })
 
-        it("ошибка валидации при создании нового пользователя", async () => {
-            userService.createUser.mockResolvedValue(mockAuthDto)
-
-            const result = await controller.create(mockUserDto)
-
-            expect(userService.createUser).toHaveBeenCalledWith(mockUserDto)
-            expect(result).toEqual(mockAuthDto)
-        })
+      it("ошибка валидации пароль число", async () => {
+        const invalidUserDto = { ...mockUserDto, password: 11 }
+        await expect(validationPipe.transform(invalidUserDto, { type: "body", metatype: UserDto }))
+          .rejects
+          .toThrow(BadRequestException)
+      })
     })
+  })
 
-    describe("обновление пользователя (полная перезапись всех свойств)", () => {
-        it("обновить данные пользователя", async () => {
-            userService.updateUser.mockResolvedValue(mockUser)
+  /*
+  describe("обновление пользователя (полная перезапись всех свойств)", () => {
+      it("обновить данные пользователя", async () => {
+          userService.updateUser.mockResolvedValue(mockCurrentUser)
 
-            const result = await controller.update(currentUserId, mockUserDto, mockUser)
+          const result = await controller.update(currentUserId, mockUserDto, mockCurrentUser)
 
-            expect(userService.updateUser).toHaveBeenCalledWith(currentUserId, mockUserDto)
-            expect(result).toEqual(mockUser)
-        })
+          expect(userService.updateUser).toHaveBeenCalledWith(currentUserId, mockUserDto)
+          expect(result).toEqual(mockCurrentUser)
+      })
 
-        it("ошибка при попытке обновить чужой профиль", async () => {
-            const otherUser = createMockUser(otherUserId)
+      it("ошибка при попытке обновить чужой профиль", async () => {
+          const otherUser = createMockUser(otherUserId)
 
-            await expect(controller.update(currentUserId, mockUserDto, otherUser))
-                .rejects
-                .toThrow(ForbiddenException)
-        })
-    })
+          await expect(controller.update(currentUserId, mockUserDto, otherUser))
+              .rejects
+              .toThrow(ForbiddenException)
+      })
+  })
 
-    describe("обновление пользователя (обновление только переданных свойств)", () => {
-        it("пользователь обновляет свой профиль", async () => {
-            const partialUpdateDto: Partial<UserDto> = {
-                name: "Updated Name"
-            }
-            const updatedUser = {
-                ...createMockUser(currentUserId),
-                name: "Updated Name"
-            } as User
-            userService.updateUser.mockResolvedValue(updatedUser)
+  describe("обновление пользователя (обновление только переданных свойств)", () => {
+      it("пользователь обновляет свой профиль", async () => {
+          const partialUpdateDto: Partial<UserDto> = {
+              name: "Updated Name"
+          }
+          const updatedUser = {
+              ...createMockUser(currentUserId),
+              name: "Updated Name"
+          } as User
+          userService.updateUser.mockResolvedValue(updatedUser)
 
-            const result = await controller.partialUpdate(currentUserId, partialUpdateDto as UserDto, mockUser)
+          const result = await controller.partialUpdate(currentUserId, partialUpdateDto as UserDto, mockCurrentUser)
 
-            expect(userService.updateUser).toHaveBeenCalledWith(currentUserId, partialUpdateDto)
-            expect(result).toEqual(updatedUser)
-        })
+          expect(userService.updateUser).toHaveBeenCalledWith(currentUserId, partialUpdateDto)
+          expect(result).toEqual(updatedUser)
+      })
 
-        it("ошибка при попытке частично обновить чужой профиль", async () => {
-            const otherUser = createMockUser(otherUserId)
-            const partialUpdateDto: Partial<UserDto> = {
-                name: "Updated Name"
-            }
+      it("ошибка при попытке частично обновить чужой профиль", async () => {
+          const otherUser = createMockUser(otherUserId)
+          const partialUpdateDto: Partial<UserDto> = {
+              name: "Updated Name"
+          }
 
-            await expect(controller.partialUpdate(currentUserId, partialUpdateDto as UserDto, otherUser))
-                .rejects
-                .toThrow(ForbiddenException)
-        })
-    })
+          await expect(controller.partialUpdate(currentUserId, partialUpdateDto as UserDto, otherUser))
+              .rejects
+              .toThrow(ForbiddenException)
+      })
+  })*/
 })
