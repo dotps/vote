b080bb28bde9a3cdb8d337a437307edc2c5a29c1
Stas Petrov <petrovstas@mail.ru>
2025-06-20 14:30:14 +0500

    fix: remove unused app service & controller #1

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import {Module} from "@nestjs/common"
-import {AppController} from "./app.controller"
-import {AppService} from "./app.service"
 import {databaseProvider} from "./providers/database.provider"
 import {configModule} from "./config/config"
 import {UserModule} from "./users/user.module"
@@ -17,9 +15,7 @@ import {SurveysModule} from "./surveys/surveys.module"
         UserModule,
         SurveysModule,
     ],
-    controllers: [AppController],
     providers: [
-        AppService,
         {
             provide: APP_GUARD,
             useClass: AuthGuard,
